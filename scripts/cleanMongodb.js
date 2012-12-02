6f88ece7fa135466e81d0308915e087e90375234
Tugdual de Kerviler <dekervit@gmail.com>
2012-12-02 19:28:18 +0100

    script cleanMongodb: empty collections instead of droping database

--- scripts/cleanMongodb.js
+++ scripts/cleanMongodb.js
@@ -4,12 +4,44 @@ var args        = process.argv.splice(2)
   , fs          = require('fs')
   , userconfig  = JSON.parse(fs.readFileSync(configFile, 'utf8'))
   , config      = require('../config')(userconfig)
-  , mongo       = require(application_root+"/lib/mongo");
+  , mongo       = require(application_root+"/lib/mongo")
+  , Step        = require('step');
 
-mongo.autoload({}, config.database.mongo, function(err, db) {
-    db.dropDatabase(function(err, done) {
-        console.log(err || "Database "+((!done)?"NOT ":"")+"droped !");
-        process.exit(0);
+
+function dropCollections(db) {
+    db.collectionNames(function(err, names) {
+        if(err || !names) {
+            console.log("No collection found");
+            process.exit(0);
+        } else {
+            names = names.map(function(colObj) { return colObj.name.split('.').splice(1).join(); });
+            Step(function() {
+                var group = this.group();
+                var groups = {};
+                names.forEach(function(name) {
+                    if(name.indexOf("system") != 0) {
+                        groups[name] = group();
+                        db.collection(name, function(err, collection) {
+                            console.log("Removing all objects in "+collection.collectionName);
+                            collection.remove({}, groups[collection.collectionName]);
+                        });
+                    }
+                });
+            }, function() {
+                process.exit(0);
+            });
+        }
     });
+}
+
+var model = {};
+mongo.autoload(model, config.database.mongo, function(err, db) {
+    if(model.mongoose.readyState == 1) {
+        dropCollections(db);
+    } else {
+        model.mongoose.on('open', function() {
+            dropCollections(db);
+        });
+    }
 });
 
